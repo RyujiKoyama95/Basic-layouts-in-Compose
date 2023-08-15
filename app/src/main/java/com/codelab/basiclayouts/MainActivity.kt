d30b9db2732ddac1c6ed51d76b668d692735afec
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-08-15 09:45:15 +0900

    Add FavoriteCollectionsGrid composable

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -35,6 +35,9 @@ import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
@@ -184,7 +187,22 @@ fun AlignYourBodyRow(
 fun FavoriteCollectionsGrid(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(120.dp),
+        content = {
+                  this.items(favoriteCollectionsData) { item ->
+                      FavoriteCollectionCard(
+                          drawable = item.drawable,
+                          text = item.text,
+                          modifier = Modifier.height(56.dp)
+                      )
+                  }
+        },
+    )
 }
 
 // Step: Home section - Slot APIs
